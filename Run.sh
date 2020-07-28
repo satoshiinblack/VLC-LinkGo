00550e405b3ea361af10cd079fafaf2f13e26336
weichih25 <weichih25@gmail.com>
2020-07-28 08:04:31 +0800

    Run.sh updated

--- Run.sh
+++ Run.sh
@@ -1,15 +1,16 @@
 #!/bin/sh
 
-#run server
-gnome-terminal -e "python3 /home/user/VLC-LinkGo/Server.py"
-#/usr/local/bin/python3 Server.py
-
-#run client
-#gnome-terminal -e "python3 /home/user/VLC-LinkGo/Client.py"
-#/usr/local/bin/python3 Client.py
-
 #run VLC
 gnome-terminal -e "vlc /home/user/VLC-LinkGo/movie.mp4 -f --loop --http-password 1234"
 #open -a VLC.app --args /Users/lumeiying/VLC-LinkGo/movie.mp4 -f --loop --http-password 1234
+
+#run server script
+gnome-terminal -e "python3 /home/user/VLC-LinkGo/Server.py"
+#/usr/local/bin/python3 Server.py
+
+#run client script
+#gnome-terminal -e "python3 /home/user/VLC-LinkGo/Client.py"
+#/usr/local/bin/python3 Client.py
+
 
 exit 0
